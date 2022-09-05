a1a38db165fc65257d4d81f1f4d0dc838599bb75
TGacek89 <gaco.g4@gmail.com>
2022-09-05 12:21:37 +0200

    first commit

---
+++ src/components/board.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./board.css"
+
+export default function board() {
+  return (
+    <main id="site main"><h1 className="text-dark.title">Birthdays!!!</h1></main>
+  )
+}
